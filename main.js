e244ce24926cc32a27b164286cec788c1461b7be
Leonel Neto <66287569+Leoleoneto@users.noreply.github.com>
2023-10-29 22:11:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -24,3 +24,10 @@ const createClient = (client) => {
     localStorage.setItem ('db_client', JSON.stringify(client))
 
 }
+
+// Eventos
+    document.getElementById('cadastrarCliente')
+        .addEventListener('click', openModal)
+    
+    document.getElementById('modalClose')
+    .addEventListener('click', closeModal)
